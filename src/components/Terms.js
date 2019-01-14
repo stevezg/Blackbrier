48137fc981e054b10771b2ebf66bc6c88bee813f
Stephen Anderson <anderssj@usc.edu>
2019-01-14 14:19:17 -0800

    add info

---
+++ src/components/Terms.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+
+export default class Terms extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to Prestige Worldwide App Inc.
+Last updated: Janurary 11, 2019
+
+//TODO make date dynamic
+
+<p>Please read these Terms of Service carefully before using the website and mobile application operated by Prestige WorldwideApp.</p>
+
+<p>When creating the account, you will be redirected to acknowledge the Terms and Privacy Policy. Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+</p>
+<p>The following Terms govern the use of Service. As used herein, the Application is defined to include the Service as well as all components thereof and all updates, patches, fixes, modifications and enhancements thereto, including releases of new versions, whether provided to you via download, automatically without additional consent or action on your part or otherwise, and any and all accompanying documentation, files and materials. The Service is owned and operated by Prestige Worldwide App and is offered subject to your acceptance without modification of all of the Terms contained herein and all other operating rules, policies and procedures that may be published from time to time by Prestige Worldwide App (collectively, the “the Agreement”).
+</p>
+<p>Please read this Agreement carefully before accessing or using the Service. By accessing or using any part of the Service platform, you agree to become bound by the terms and conditions of these Terms. If you do not agree to all the terms and conditions in these Terms, then you should not access or use the Service.</p>
+
+
+
+
+
+
+<p>By making Content available, you represent and warrant that:</p>
+<p>The downloading, copying and use of the Content will not infringe the proprietary rights, including but limited to the copyright, patent, trademark or trade secret rights, of any third party;
+You have fully complied with any third-party licenses relating to the Content used by you to create and organize experiences, and have done all things necessary to successfully pass through the end users any required terms;
+</p>
+<p>The Content does not contain or install any viruses, malware or any other files or programs designed to interrupt, destroy or limit the functionality of any computer or hardware;
+The Content is not spam, is not machine generated, and does not contain unethical or unwanted commercial content designed to drive traffic to third party sites or boost the search engine rankings of third party sites, or to further unlawful acts or mislead recipients as to the source of the material;</p>
+
+<p>The Content is not unlawful, harmful, threatening, abusive, tortious, defamatory, libelous, vulgar, obscene, lewd, profane, invasive of another’s privacy, hateful, or racially, ethnically or otherwise objectionable;</p>
+
+
+<p>Your account is not getting advertised via unwanted electronic messages such as spam links on newsgroups, email lists, other blogs and web sites, and similar unsolicited promotional methods;
+Your account is not named in a manner that misleads your readers into thinking that you are another person or company.
+Content</p>
+
+<p>Content Posted on Other Websites and Apps
+Prestige Worldwide App has not reviewed, and cannot review, all of the material made available through the Prestige Worldwide App website and apps to which myPrestige Worldwide App.com link. We do not have any control over those non-Prestige Worldwide App websites, webpages and apps, and we are not responsible for their contents, privacy policies or practices. By linking to a non-Prestige Worldwide App website, webpage or app, We do not represent or imply that we endorse such website or webpage. You are responsible for taking precautions as necessary to protect yourself and your computer systems from viruses and other harmful or destructive content that might exist on those websites, webpages and/or apps. Prestige Worldwide App disclaims any responsibility for any harm resulting from, or in connection with your use of or reliance on any non-Prestige Worldwide App websites, webpages and apps.
+</p>
+
+<p>Limitations
+You may not use the Application to create inappropriate, violent, sexual or illegal experiences. Prestige Worldwide App reserves the right to prohibit users from soliciting commercial services from other users or from positing inappropriate Content on other User’s cards. Users are also prohibited from screenshotting any Content or any part of the Application, including publicly exposing the experiences which are being planned for, or that have been completed by other users. If Prestige Worldwide App determines or believes, in our sole discretion, that you have violated or attempted to violate these terms and conditions based on these Terms, your ability to use and access the Application may be temporarily or permanently revoked, with or without notice.
+</p>
+<p>Data Collection and Use
+You hereby agree that Prestige Worldwide App, our affiliates, and our respective successors and assigns, may collect and use certain technical information associated with your use of the Application, including, without limitation, any information provided in connection with any support or technical services for the Application.
+</p>
+<p>Changes to Terms
+We may at any time and from time to time modify, discontinue, temporarily or permanently, the Application, or any portion thereof, with or without notice. As part of Prestige Worldwide App’s platform, you may from time to time receive updates to the Prestige Worldwide App which may be automatically downloaded and installed to your device. These updates may include bug fixes, feature enhancement and other improvements, or an entirely new version of the Application. You agree that Prestige Worldwide App may deliver such updates to you as part of the Service, which you shall install them as required. You further agree that Prestige Worldwide App shall not be liable to you or any third-party for such updates, modification, or discontinuance of the Application.
+</p>
+<p>Disclaimer of Warranties; Limitation of Liability
+WE DO NOT GUARANTEE, REPRESENT OR WARRANT THAT YOUR USE OF OUR SERVICE WILL BE UNINTERRUPTED, TIMELY, SECURE OR ERROR-FREE. WE DO NOT WARRANT THAT THE RESULTS THAT MAY BE OBTAINED FROM THE USE OF THE SERVICE WILL BE ACCURATE OR RELIABLE. YOU AGREE THAT FROM TIME TO TIME WE MAY REMOVE THE SERVICE FOR INDEFINITE PERIODS OF TIME OR CANCEL THE SERVICE AT ANY TIME, WITHOUT NOTICE TO YOU. YOU EXPRESSLY AGREE THAT YOUR USE OF, OR INABILITY TO USE, THE SERVICE IS AT YOUR SOLE RISK. THE SERVICE AND ALL PRODUCTS AND SERVICES DELIVERED TO YOU THROUGH THE SERVICE ARE (EXCEPT AS EXPRESSLY STATED BY US) PROVIDED ‘AS IS’ AND ‘AS AVAILABLE’ FOR YOUR USE, WITHOUT ANY REPRESENTATION, WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, MERCHANTABLE QUALITY, FITNESS FOR A PARTICULAR PURPOSE, DURABILITY, TITLE, AND NON-INFRINGEMENT.</p>
+
+<p>IN NO CASE SHALL Prestige Worldwide APP, OUR , OFFICERS, EMPLOYEES, AGENTS, LICENSORS, REPRESENTATIVES, ADVERTISERS, BUSINESS AND PROMOTIONAL PARTNERS, OPERATIONAL SERVICE PROVIDERS, AND CONTRACTORS BE LIABLE FOR ANY INJURY, LOSS, CLAIM, OR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL, OR CONSEQUENTIAL DAMAGES OF ANY KIND, INCLUDING, WITHOUT LIMITATION LOST PROFITS, LOST REVENUE, LOST SAVINGS, LOSS OF DATA, REPLACEMENT COSTS, OR ANY SIMILAR DAMAGES, WHETHER BASED IN CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, ARISING FROM YOUR USING THE SERVICE, OR FOR ANY OTHER CLAIM RELATED IN ANY WAY TO YOUR USE OF THE SERVICE, INCLUDING, BUT NOT LIMITED TO, ANY ERRORS OR OMISSIONS IN ANY CONTENT, OR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SERVICE OR ANY CONTENT POSTED, TRANSMITTED, OR OTHERWISE MADE AVAILABLE VIA THE SERVICE, EVEN IF ADVISED OF THEIR POSSIBILITY. BECAUSE SOME STATES OR JURISDICTIONS DO NOT ALLOW THE EXCLUSION OR THE LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES, IN SUCH STATES OR JURISDICTIONS, OUR LIABILITY SHALL BE LIMITED TO THE MAXIMUM EXTENT PERMITTED BY LAW.
+</p>
+<p>General Representation and Warranty
+You represent and warrant that your use of the Service will be in strict accordance with all applicable laws and regulations (including without limitation any local laws or regulations in your country, state, city, or other governmental area, regarding online conduct and acceptable content, and including all applicable laws regarding the transmission of technical data exported from the United States or the country in which you reside).
+</p>
+
+
+<p>Termination
+Prestige Worldwide App reserves the right to terminate or suspend your access to our Service at any time for any reason, including in the event Prestige Worldwide App believes or has reason to believe that you have failed to comply with these Terms. If Prestige Worldwide App terminates your right to use the Application, you must stop all use of the Application.
+</p>
+<p>Entire Agreement
+The failure of us to exercise or enforce any right or provision of these Terms shall not constitute a waiver of such right or provision. These Terms and any policies or operating rules posted by us on this site or in respect to the Service constitutes the entire agreement and understanding between you and us and governs your use of the Service, superseding any prior or contemporaneous agreements, communications and proposals, whether oral or written, between you and us (including, but not limited to, any prior versions of the Terms). Any ambiguities in the interpretation of these Terms shall not be construed against the drafting party.
+</p>
+
+
+<p>Prestige Worldwide
+Aspen, CO
+</p>
+      </div>
+    )
+  }
+}
+
+
+
---
+++ src/components/what.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./whatStyle.css"
+
+export default class what extends Component {
+  render() {
+    return (
+      <div className="what">
+        <h1>What is Prestige Worldwide?</h1>
+        <p>
+          Prestige Worldwide is a decentralized peer to peer real estate
+          platform
+        </p>
+        <p>
+        100% electronic and secure property sale, elimintating fraud.
+        </p>
+        <p>Digital contracts which automatically withdraw mortgage payments and service charges with a full audit trail, immutable. </p>
+
+        <p>Smart contracts execute themselves automatically once the requirements for the contract are met. The terms of the smart contract are transparent to everyone on the network, they are immutable and permanent. </p>
+      </div>
+    )
+  }
+}
